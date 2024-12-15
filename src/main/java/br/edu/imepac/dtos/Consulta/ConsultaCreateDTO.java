f100f5b2ae8f7a2141ff89fd7654577f489525bd
GussoniErisson <168599540+GussoniErisson@users.noreply.github.com>
2024-12-15 18:23:25 -0300

    Update ConsultaCreateDTO.java

--- src/main/java/br/edu/imepac/dtos/Consulta/ConsultaCreateDTO.java
+++ src/main/java/br/edu/imepac/dtos/Consulta/ConsultaCreateDTO.java
@@ -1,21 +1,29 @@
 package br.edu.imepac.dtos.Consulta;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import br.edu.imepac.entidades.Funcionario;
+import br.edu.imepac.entidades.Paciente;
+import jakarta.persistence.*;
+import lombok.*;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//@JsonInclude(JsonInclude.Include.NON_NULL)
 
-public class ConsultaCreateDTO {
+public class ConsultaDTO{
 
     private LocalDateTime dataHorario;
     private String sintomas;
     private boolean eRetorno;
     private boolean estaAtiva;
 
+    private long medico;
+
+    private long atendente;
+
+    private long paciente;
+
+    private String prontuario;
+
+    private String convenio;
 }
