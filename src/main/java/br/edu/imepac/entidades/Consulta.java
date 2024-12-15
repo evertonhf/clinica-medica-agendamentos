d26d92cc0441cfd036dc2739c901107d86fd5a63
GussoniErisson <erisson.gussoni@aluno.imepac.edu.br>
2024-12-15 11:55:52 -0300

    Entidade Consulta

---
+++ src/main/java/br/edu/imepac/entidades/Consulta.java
@@ -0,0 +1,44 @@
+package br.edu.imepac.entidades;
+
+import lombok.Data;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "consulta")
+@Entity
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private LocalDateTime dataHorario;
+    private String sintomas;
+    private boolean eRetorno;
+    private boolean estaAtiva;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "medico_id")
+    private Funcionario medico;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "atendente_id")
+    private Funcionario atendente;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    //@ManyToOne(mappedBy = "prontuario", cascade = CascadeType.REMOVE)
+    private String prontuario;
+
+    //@OneToOne(mappedBy = "convenio", cascade = CascadeType.REMOVE)
+    private String convenio;
+}
